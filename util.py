200a82074578ad55fe1f96db59a01593e7881d62
Alex Kunin <ascii.rock@gmail.com>
2021-04-16 11:35:15 -0500

    added simplify_type

--- util.py
+++ util.py
@@ -3,9 +3,10 @@ utility functions for flybrain clustering
 """
 import datetime
 import sys
-
+import re
 
 # from scipy import sparse
+
 
 def log_msg(*args, out=sys.stdout, **kwargs):
     """Print message m with a timestamp if out is not None."""
@@ -15,3 +16,17 @@ def log_msg(*args, out=sys.stdout, **kwargs):
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
+
+
+def simplify_type(s):
+    """
+    Parses a cell type from the hemibrain database and extracts the first few
+    letters or returns 'part' if the name starts with '('
+    """
+    if s[0] == '(':
+        return 'part'
+    elif s == "None":
+        return s
+    else:
+        m = re.search(r'([A-Z]+)(.*)', s)
+        return m.group(1)
